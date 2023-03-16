7376f45c3dfc5b0edf5ef5f4d85ad1944ca4b8f0
CMBBauc <63628375+CMBBauc@users.noreply.github.com>
2023-03-16 02:42:18 -0500

    Added components and assets folder, updated index.html with new meta tags

---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import Logo from "./../assets/Logo .jpg";
+
+function Nav() {
+    return(
+        <nav>
+            <img src={Logo}alt="little lemon logo"></img>
+            <ul>
+                <li><a>Home</a></li>
+                <li><a>About</a></li>
+                <li><a>Menu</a></li>
+                <li><a>Reservations</a></li>
+                <li><a>Online Order</a></li>
+                <li><a>Login</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Nav;
